c5785a27c863bffb58f0db131ec909c14b5fd9de
Juan Camilo <jbetancurrico@gmail.com>
2022-03-18 21:05:31 -0500

    validation_path

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #Python
+from msilib.schema import Patch
 from typing import Optional
 #Pydantic
 from pydantic import BaseModel
 #FastAPI
-from fastapi import FastAPI, Query
-from fastapi import Body
-from fastapi import Query
+from fastapi import FastAPI, Body, Query, Path
 
 app= FastAPI()
 
@@ -31,16 +30,37 @@ def create_person(person: Person = Body(...)):
 
 @app.get("person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    name: Optional[str] = Query(
+        None, 
+        min_length=1, 
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters"
+        ),
      # El ... para hacerlo obligatorio, no recomendado en un Query parameter
-
-    age: int = Query(...)
+    age: int = Query(
+        ...,
+        title= "Person Age",
+        description= "This is the person age. It's required"
+        )
 ):
     return {name: age}
-    
+
     '''
     Funcion para probar la validacion en los query parameter.
     El Age esta obligatorio, no se recomienda hacer esto, si necesitas un
     parametro obligatorio se recomienda hacerlo en un path parameter
     '''
-   
+
+# Validaciones: Path Parametros
+    
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title= "Detail Person",
+        description= "This is the detail person."
+        )
+    ):
+        return  {person_id: "It exists!"}
